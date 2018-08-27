1a72bfb99fc5d31a96ae49cd3a45c92dba970f8b
sj216 <786212822@Qq.com>
2018-08-27 22:51:56 +0800

    歌手列表按钮添加，为单曲播放页做vuex存值准备

--- src/components/music-list/music-list.vue
+++ src/components/music-list/music-list.vue
@@ -1,10 +1,16 @@
 <template>
   <div class="music-list">
-    <div class="back">
+    <div class="back" @click="back">
       <i class="icon-back"></i>
     </div>
     <h1 class="title" v-html="title"></h1>
     <div class="bg-image" :style="bgStyle" ref="bgImage">
+      <div class="play-wrapper">
+        <div class="play" v-show="songs.length > 0" ref="playBtn">
+          <i class="icon-play"></i>
+          <span class="text">随机播放全部</span>
+        </div>
+      </div>
       <div class="filter" ref="filter"></div>
     </div>
     <div class="bg-layer" ref="layer"></div>
@@ -20,6 +26,9 @@
           :songs="songs"
         />
       </div>
+      <div class="loading-container" v-show="!songs.length">
+        <loading/>
+      </div>
     </scroll>
   </div>
 </template>
@@ -27,14 +36,20 @@
 <script>
 import Scroll from '@/base/scroll/scroll'
 import SongList from '@/base/song-list/song-list'
+import Loading from '@/base/loading/loading'
+import { prefixStyle } from '../../common/js/dom'
 
 const RESERVED_HEIGHT = 40
+
+const transform = prefixStyle('transform')
+const backdrop = prefixStyle('backdrop-filter')
 
 export default {
   name: 'music-list',
   components: {
     Scroll,
-    SongList
+    SongList,
+    Loading
   },
   data() {
     return {
@@ -78,8 +93,7 @@ export default {
       let scale = 1
       // 下拉过程中有一个模糊的效果
       let blur = 0
-      this.$refs.layer.style['transform'] = `translate3d(0,${translateY}px,0)`
-      this.$refs.layer.style['webkitTransform'] = `translate3d(0,${translateY}px,0)` // 设置浏览器
+      this.$refs.layer.style[transform] = `translate3d(0,${translateY}px,0)`
       // bglayer的高度是100%，就是屏幕的高度
       // 我们需要做限制让bglayer不是无限制的永远滚动，而是滚动到一定高度之后就不滚动了，因此需要设置最大的滚动距离
       const percent = Math.abs(newY / this.imageHeight)
@@ -89,19 +103,19 @@ export default {
       } else {
         blur = Math.min(20 * percent, 20) // 向上滑动的时候有高斯模糊的效果
       }
-      this.$refs.filter.style['backdrop-filter'] = `blur(${blur})` // 这个属性只有ios能看到
-      this.$refs.filter.style['webkitBackdrop-filter'] = `blur(${blur})` // 这个属性只有ios能看到
+      this.$refs.filter.style[backdrop] = `blur(${blur})` // 这个属性只有ios能看到
       if (newY < this.minTranslateY) {
         zIndex = 10
         this.$refs.bgImage.style.paddingTop = 0
         this.$refs.bgImage.style.height = `${RESERVED_HEIGHT}px`
+        this.$refs.playBtn.style.display = 'none'
       } else {
         this.$refs.bgImage.style.paddingTop = '70%'
         this.$refs.bgImage.style.height = 0
+        this.$refs.playBtn.style.display = ''
       }
       this.$refs.bgImage.style.zIndex = zIndex
-      this.$refs.bgImage.style['transform'] = `scale(${scale})`
-      this.$refs.bgImage.style['webkitTransform'] = `scale(${scale})` // 设置浏览器
+      this.$refs.bgImage.style[transform] = `scale(${scale})`
     }
   },
   computed: {
@@ -112,6 +126,9 @@ export default {
   methods: {
     scroll(pos) {
       this.scrollY = pos.y // 实时拿到scrollY的值
+    },
+    back() {
+      this.$router.back()
     }
   }
 }
--- src/store/state.js
+++ src/store/state.js
@@ -1,5 +1,13 @@
+import {playMode} from '@/common/js/config'
+
 const state = {
-  singer: {}
+  singer: {},
+  playing: false, // 播放器的两个播放状态  播放和暂停
+  fullScreen: false, // 是否全屏
+  playList: [], // 播放列表
+  sequenceList: [], // 顺序列表
+  mode: playMode.sequence, // 设置播放模式
+  currentIndex: -1 // 当前播放歌曲的索引值
 }
 
 export default state
